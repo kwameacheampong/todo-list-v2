fe34bd7d834bb69c88a8821889d0dd386c7995a6
Ubuntu <ignat@development-server-jenkins.z0wdas3oujmethwmnw2lndpdda.fx.internal.cloudapp.net>
2021-11-29 19:45:45 +0000

    adding deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,4 +3,4 @@
 echo "Deploy stage"
 
 
-ssh jenkins@develop-server-jenkins docker stack deploy --compose-file docker-compose.yaml todo-app
+ssh jenkins@development-server-jenkins docker stack deploy --compose-file docker-compose.yaml todo-app
