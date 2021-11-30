37236ff879fd32470db0f93ea8d17ff8117401e4
Ubuntu <ignat@development-server-jenkins.z0wdas3oujmethwmnw2lndpdda.fx.internal.cloudapp.net>
2021-11-30 09:43:04 +0000

    adding to the setup

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y curl jq python3 -venv
 if [ ! -f "/usr/bin/docker" ]; then
    curl https://get.docker.com | sudo bash
    sudo usermod -aG docker jenkins
+fi
 
 # install docker compose 
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
